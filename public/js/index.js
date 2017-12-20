8dfdf759e16a7ae5a0663cc618d8ff12f5220007
Mikhail Grekov <mishagrekov060193@gmail.com>
2017-12-20 15:52:05 +0300

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,54 @@
-let socket = io();
+var socket = io();
 
-socket.on('connect', () => {
+socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', () => {
+socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', (message) => {
+socket.on('newMessage', function (message) {
   console.log('newMessage', message);
-  let li = $('<li></li>');
+  var li = $('<li></li>');
   li.text(`${message.from}: ${message.text}`);
 
   $('#messages').append(li);
 });
 
-$('#message-form').on('submit', (e) => {
+socket.on('newLocationMessage', function (message) {
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">Мое текущее местоположение</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function (e) {
   e.preventDefault();
 
   socket.emit('createMessage', {
     from: 'User',
     text: $('[name=message]').val()
-  }, () => {
+  }, function () {
 
+  });
+});
+
+var locationButton = $('#send-location');
+$('#send-location').on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Геолокация не поддерживается вашим браузером.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Не удалось найти местоположение.');
   });
 });
